52e366fe1a237c6f53a7829567a031781c1eec4c
GukbinIm <bill2024220031@cau.ac.kr>
2025-07-14 12:29:33 +0900

    Update SC-IAT.js

--- SC-IAT.js
+++ SC-IAT.js
@@ -1592,7 +1592,6 @@ function main_loopRoutineBegin(snapshot) {
     main_loopMaxDurationReached = false;
     // update component parameters for each repeat
     // Run 'Begin Routine' code from code_5
-    // import * as pd from 'pandas';
     df = pd.read_excel("stimuli.xlsx");
     subset_df_1 = df.sample({"n": 24, "replace": false}).reset_index({"drop": true});
     subset_df_1.to_csv("temp_conditions.csv", {"index": false});
